92d4d91ece578f59c7f318c331890635430afbf8
Marcus Vinje Johansen <tex@duck.com>
2023-04-24 14:00:33 +0200

    Initial vite setup, with empty components

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
+import Project from "./Components/Project/Project";
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Hero />
+            <About />
+            <Project />
+            <Contact />
+            <Footer />
+        </>
+    );
+};
+
+export default App;

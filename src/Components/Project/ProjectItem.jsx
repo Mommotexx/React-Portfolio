e21fe79ed1589c42476b51a273d012ee2588a1c8
Marcus Vinje Johansen <tex@duck.com>
2023-05-09 10:16:33 +0200

    Added more styling to the project cards. Now with background images

--- src/Components/Project/ProjectItem.jsx
+++ src/Components/Project/ProjectItem.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const ProjectItem = ({
     title,
     shortDescription,
@@ -5,26 +7,42 @@ const ProjectItem = ({
     demo,
     background,
 }) => {
+    const [showBg, setShowBg] = useState(true);
     return (
-        <article className="project_item flex flex-col w-full items-center md:w-[30%] bg-[#0a6d1f] text-white py-6 rounded-lg">
-            <div className="flex mb-2">
-                {technologies.map((tech) => (
-                    <img
-                        key={"Title " + tech}
-                        src={tech}
-                        width={32}
-                        className="saturate-0 mr-2"
-                    />
-                ))}
-            </div>
-            <h1 className="font-opensans">{title}</h1>
-            <p className="font-opensans mb-2">{shortDescription}</p>
-            <a
-                href={demo}
-                className=" border-green border-[1.5px] p-2 rounded-2xl"
+        <article
+            className={`project_item flex flex-col w-full items-center md:w-[30%] min-h-[200px] text-white bg-[#0c6b03]  py-6 rounded-lg ${
+                showBg &&
+                `bg-[url('${background}')] bg-cover bg-no-repeat bg-left bg-center`
+            } `}
+            onFocus={() => setShowBg(false)}
+            onMouseEnter={() => setShowBg(false)}
+            onMouseLeave={() => setShowBg(true)}
+        >
+            {showBg && <h2>{title}</h2>}
+            <div
+                className={`flex flex-col w-full items-center -z-20 ${
+                    !showBg && `z-30`
+                }`}
             >
-                Visit Website
-            </a>
+                <div className="flex mb-2">
+                    {technologies.map((tech) => (
+                        <img
+                            key={"Title " + tech}
+                            src={tech}
+                            width={32}
+                            className="saturate-0 mr-2"
+                        />
+                    ))}
+                </div>
+                <h1 className="font-opensans">{title}</h1>
+                <p className="font-opensans mb-2">{shortDescription}</p>
+                <a
+                    href={demo}
+                    className=" border-green border-[1.5px] p-2 rounded-2xl"
+                >
+                    Visit Website
+                </a>
+            </div>
         </article>
     );
 };

c2031f6990f631f2d9a6f7b44e16d1b9dc0c342a
Marcus Vinje Johansen <tex@duck.com>
2023-05-10 15:46:44 +0200

    Added footer. Not sure what to have in my footer, but consider this done now

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,41 @@
+import { FaLinkedinIn, FaGithub, FaGitlab } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className="flex flex-col lg:w-full m-auto items-center h-[30vh] bg-dark">
+            <div className="flex items-center justify-center m-auto gap-5 flex-1">
+                <a
+                    href="https://www.linkedin.com/in/marcusvinje/"
+                    target="_blank"
+                    className="p-3 rounded-full bg-green hover:bg-dark hover:text-white hover:border-green hover:border"
+                >
+                    <FaLinkedinIn />
+                </a>
+                <a
+                    href="https://github.com/Mommotexx"
+                    target="_blank"
+                    className="p-3 rounded-full bg-green hover:bg-dark hover:text-white hover:border-green hover:border"
+                >
+                    <FaGithub />
+                </a>
+                <a
+                    href="https://gitlab.com/mommotexx"
+                    target="_blank"
+                    className="p-3 rounded-full bg-green hover:bg-dark hover:text-white hover:border-green hover:border"
+                >
+                    <FaGitlab />
+                </a>
+                <a
+                    href="mailto:tex@duck.com"
+                    className="p-3 rounded-full bg-green hover:bg-dark hover:text-white hover:border-green hover:border"
+                >
+                    <MdOutlineEmail />
+                </a>
+            </div>
+            <p className="text-white mb-8">&copy;2023 Marcus Johansen</p>
+        </footer>
+    );
 };
 
 export default Footer;

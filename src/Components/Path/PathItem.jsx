77ca82d8616a224d244f66df7b7d876a7782e055
Marcus Vinje Johansen <tex@duck.com>
2023-05-19 12:54:21 +0200

    UX feedback: should not use green text on white background. Fixed

--- src/Components/Path/PathItem.jsx
+++ src/Components/Path/PathItem.jsx
@@ -4,11 +4,17 @@ const PathItem = ({ title, icon, description, technologies, devTools }) => {
     return (
         <article className="flex flex-col items-center text-center md:w-2/6 h-fit">
             {icon}
-            <h1 className="font-opensans font-bold mt-2">{title}</h1>
+            <h1 className="font-opensans font-bold mt-2 text-[#0a0a0a] text-xl">
+                {title}
+            </h1>
             <p className="mt-6 md:min-h-[80px]">{description}</p>
-            <p className="mt-8 text-green font-semibold">Technologies I use:</p>
+            <p className="mt-8 text-[#3e3e3e] font-semibold text-lg">
+                Technologies I use:
+            </p>
             <p>{technologies.join(", ")} </p>
-            <p className="mt-8 text-green font-semibold">Dev tools:</p>
+            <p className="mt-8 text-[#3e3e3e] font-semibold text-lg">
+                Dev tools:
+            </p>
             <ul>
                 {devTools.map((tool) => (
                     <li key={tool}>{tool}</li>

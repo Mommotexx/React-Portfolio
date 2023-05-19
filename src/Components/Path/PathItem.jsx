52d3535c5150b2d9d7dc13cdf729248cc97ef009
Marcus Vinje Johansen <tex@duck.com>
2023-05-19 13:12:51 +0200

    UX feedback: unify fonts

--- src/Components/Path/PathItem.jsx
+++ src/Components/Path/PathItem.jsx
@@ -4,17 +4,15 @@ const PathItem = ({ title, icon, description, technologies, devTools }) => {
     return (
         <article className="flex flex-col items-center text-center md:w-2/6 h-fit">
             {icon}
-            <h1 className="font-opensans font-bold mt-2 text-[#0a0a0a] text-xl">
-                {title}
-            </h1>
+            <h1 className=" font-bold mt-2 text-[#0a0a0a] text-xl">{title}</h1>
             <p className="mt-6 md:min-h-[80px]">{description}</p>
-            <p className="mt-8 text-[#3e3e3e] font-semibold text-lg">
+            <h2 className="mt-8 text-[#3e3e3e] font-semibold text-lg">
                 Technologies I use:
-            </p>
+            </h2>
             <p>{technologies.join(", ")} </p>
-            <p className="mt-8 text-[#3e3e3e] font-semibold text-lg">
+            <h2 className="mt-8 text-[#3e3e3e] font-semibold text-lg">
                 Dev tools:
-            </p>
+            </h2>
             <ul>
                 {devTools.map((tool) => (
                     <li key={tool}>{tool}</li>

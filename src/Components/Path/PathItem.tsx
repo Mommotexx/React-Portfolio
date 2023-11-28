2b809a9b43b6a8cf02da52bc5b5acb2d85c6968a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-11-28 23:18:28 +0100

    Forbedring design

--- src/Components/Path/PathItem.tsx
+++ src/Components/Path/PathItem.tsx
@@ -9,23 +9,21 @@ export interface Props {
 
 const PathItem = (props:Props): JSX.Element => {
     return (
-        <article className="flex flex-col items-center text-center md:w-2/6 h-fit">
+        <article className="flex flex-col text-left md:w-2/6 h-fit">
             {props.icon}
             <h1 className=" font-bold mt-2 text-[#0a0a0a] text-xl">{props.title}</h1>
-            <p className="mt-6 md:min-h-[80px]">{props.description}</p>
-            <h2 className="mt-8 text-[#3e3e3e] font-semibold text-lg">
+            <p className="mt-4 md:min-h-[60px]">{props.description}</p>
+            <h2 className="mt-4 text-[#3e3e3e] font-semibold text-lg">
                 Technologies I use:
             </h2>
             <p>{props.technologies.join(", ")} </p>
-            <h2 className="mt-8 text-[#3e3e3e] font-semibold text-lg">
+            <h2 className="mt-6 text-[#3e3e3e] font-semibold text-lg">
                 Dev tools:
             </h2>
-            <ul>
-                {props.devTools.map((tool) => (
-                    <li key={tool}>{tool}</li>
-                ))}
-                <li className="mt-4">and more..</li>
-            </ul>
+            <div>
+            <p>{props.devTools.join(", ")} </p>
+            </div>
+           
         </article>
     );
 };

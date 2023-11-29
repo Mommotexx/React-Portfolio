e2ebffd098a778abfed9c4eeb252a0a69a419144
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-11-29 03:06:54 +0100

    Margin prosjekttittel

--- src/Components/Project/ProjectItem.tsx
+++ src/Components/Project/ProjectItem.tsx
@@ -6,7 +6,7 @@ const ProjectItem = (props: projectItemProps) => {
   return (
     <article className="project_item flex flex-col w-full items-center text-white py-6 rounded-lg bg-center bg-no-repeat bg-cover relative">
       <img src={props.background} alt={props.title} />
-      <h2 className="bg-dark rounded px-2 bg-opacity-50">{props.title}</h2>
+      <h2 className="bg-dark rounded mt-4 px-2 bg-opacity-50">{props.title}</h2>
       <div className="hover:opacity-100 bg-dark rounded-lg absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center opacity-0 transition-all ease-in-out duration-300">
         <div className="flex mb-2">
         {props.technologies.map((tech) => {
